c6ddfec8f795b75bbdd0df1fcf4ca0eb5cf2157a
6xiaowu9 <736518585@qq.com>
2021-05-22 00:31:40 +0800

    Update Log.php
    update print int|float|bool|string|object|array

--- src/Log.php
+++ src/Log.php
@@ -34,51 +34,51 @@ class Log extends Style
     public static function info($data = "")
     {
         $type = __FUNCTION__;
-        if (is_string($data)) {
-            self::stdout('> [Info][' . date('Y-m-d H:i:s') . ']: ' . $data, 0, self::MODE_DEFAULT, '', '', $type);
-        } else if (is_array($data) || is_object($data)) {
+        if (is_array($data) || is_object($data)) {
             self::formatObject($data, function (string $string = '') use ($type) {
                 self::stdout('> [Info][' . date('Y-m-d H:i:s') . ']: ' . $string, 0, self::MODE_DEFAULT, '', '', $type);
             });
+        } else {
+            self::stdout('> [Info][' . date('Y-m-d H:i:s') . ']: ' . self::formatToString($data), 0, self::MODE_DEFAULT, '', '', $type);
         }
     }
-    
+
     public static function debug($data = "")
     {
         $type = __FUNCTION__;
-        if (is_string($data)) {
-            self::stdout('> [Debug][' . date('Y-m-d H:i:s') . ']: ' . $data, 0, self::MODE_DEFAULT, self::FG_WHITE, '', $type);
-        } else if (\is_array($data) || is_object($data)) {
+        if (\is_array($data) || is_object($data)) {
             self::formatObject($data, function (string $string = '') use ($type) {
                 self::stdout('> [Debug][' . date('Y-m-d H:i:s') . ']: ' . $string, 0, self::MODE_DEFAULT, self::FG_WHITE, '', $type);
             });
+        } else {
+            self::stdout('> [Debug][' . date('Y-m-d H:i:s') . ']: ' . self::formatToString($data), 0, self::MODE_DEFAULT, self::FG_WHITE, '', $type);
         }
     }
 
     public static function warning($data = "")
     {
         $type = __FUNCTION__;
-        if (is_string($data)) {
-            self::stdout('> [Warning][' . date('Y-m-d H:i:s') . ']: ' . $data, 0, self::MODE_DEFAULT, self::FG_CYAN, '', $type);
-        } else if (\is_array($data) || is_object($data)) {
+        if (\is_array($data) || is_object($data)) {
             self::formatObject($data, function (string $string = '') use ($type) {
                 self::stdout('> [Warning][' . date('Y-m-d H:i:s') . ']: ' . $string, 0, self::MODE_DEFAULT, self::FG_CYAN, '', $type);
             });
+        } else {
+            self::stdout('> [Warning][' . date('Y-m-d H:i:s') . ']: ' . self::formatToString($data), 0, self::MODE_DEFAULT, self::FG_CYAN, '', $type);
         }
     }
 
     public static function error($data = "")
     {
         $type = __FUNCTION__;
-        if (is_string($data)) {
-            self::stdout('> [Error][' . date('Y-m-d H:i:s') . ']: ' . $data, 0, self::MODE_DEFAULT, self::FG_RED, '', $type);
-        } else if (\is_array($data) || is_object($data)) {
+        if (\is_array($data) || is_object($data)) {
             self::formatObject($data, function (string $string = '') use ($type) {
                 self::stdout('> [Error][' . date('Y-m-d H:i:s') . ']: ' . $string, 0, self::MODE_DEFAULT, self::FG_RED, '', $type);
             });
+        } else {
+            self::stdout('> [Error][' . date('Y-m-d H:i:s') . ']: ' . self::formatToString($data), 0, self::MODE_DEFAULT, self::FG_RED, '', $type);
         }
     }
-    
+
     public static function stdout($target, int $tabNum = 0, string $mode = Log::MODE_DEFAULT, string $fg = '', string $bg = '', string $type = 'info'): void
     {
         $out = Style::sprintf($target, $tabNum, $mode, $fg, $bg) . PHP_EOL;
@@ -93,10 +93,28 @@ class Log extends Style
         }
     }
 
-    protected static function formatObject($data, callable $stdout, int $tabNum = 0) 
+    protected static function formatToString($data)
     {
-        
-        $tab = ''; 
+        switch (true) {
+            case is_string($data):
+                return $data;
+                break;
+            case is_int($data) || is_float($data):
+                return (string) $data;
+                break;
+            case is_bool($data):
+                return $data ? 'true' : 'false';
+                break;
+            default:
+                return $data;
+                break;
+        }
+    }
+
+    protected static function formatObject($data, callable $stdout, int $tabNum = 0)
+    {
+
+        $tab = '';
         $delimiterStart = '[';
         $delimiterStop = ']';
 
@@ -122,13 +140,13 @@ class Log extends Style
             switch (true) {
                 case is_null($value):
                     $stdout("| " . str_pad($tab, $tabNum, "\t") . $keySymbol . $key  . $keySymbol . "\t=>\tNULL,", $tabNum);
-                break;
+                    break;
                 case is_string($value) || is_numeric($value) || is_bool($value):
                     $valueType = is_string($value) ? "\"" : "";
                     is_bool($value) ? $value == TRUE ? $value = "true" :  $value = "false" : NULL;
                     $stdout("| " . str_pad($tab, $tabNum, "\t") . $keySymbol . $key  . $keySymbol . "\t=>\t" . $valueType . $value . $valueType . ",", $tabNum);
-                break;
-                
+                    break;
+
                 case is_array($data) || is_object($data):
                     if (is_callable($value)) {
                         $stdout("| " . str_pad($tab, $tabNum, "\t") . $keySymbol . $key  . $keySymbol . "\t=>\tfunction(){},", $tabNum);
@@ -136,12 +154,12 @@ class Log extends Style
                         $stdout("| " . str_pad($tab, $tabNum, "\t") . $keySymbol . $key  . $keySymbol . "\t=>\t", $tabNum);
                         self::formatObject($value, $stdout, $tabNum);
                     }
-                break;
+                    break;
             }
         }
         $tabNum -= 1;
         $stdout("| " . str_pad($tab, $tabNum, "\t") . $delimiterStop . ($tabNum == 0 ? "" : ","), $tabNum);
-        
+
         if ($tabNum == 0) {
             $stdout("|--------------------------------------------------|");
             $stdout("|-------- Print array and object log stop  --------|");
@@ -149,15 +167,15 @@ class Log extends Style
         }
     }
 
-    protected static function get_variable_name(&$var, $scope){
-    
+    protected static function get_variable_name(&$var, $scope)
+    {
+
         $tmp = $var;
-        
-        $var = 'tmp_value_'.mt_rand();
-        $name = array_search($var, $scope, true); 
-    
+
+        $var = 'tmp_value_' . mt_rand();
+        $name = array_search($var, $scope, true);
+
         $var = $tmp;
         return $name;
-
     }
 }
